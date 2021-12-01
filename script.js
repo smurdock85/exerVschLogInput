54237ddf4b646e11c977f27020a935646fa1e423
smurdock85 <89748811+smurdock85@users.noreply.github.com>
2021-11-30 17:18:08 -0700

    done

---
+++ script.js
@@ -0,0 +1,5 @@
+var inputBox = document.getElementById("text-input")
+console.log("The button was clicked!")
+
+var button = document.getElementById("button")
+button.addEventListener("clicked", handleButtonClick)
